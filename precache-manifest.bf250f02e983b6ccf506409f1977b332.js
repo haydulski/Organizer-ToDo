931913854ccefb43220d2d9b7b784a6d9e869768
Damian <dhfxblog@gmail.com>
2020-04-04 19:38:41 +0200

    Updates

---
+++ precache-manifest.bf250f02e983b6ccf506409f1977b332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201b8f2593d1c09409b658768169d3e3",
+    "url": "/haydulski/Organizer-ToDo.git/index.html"
+  },
+  {
+    "revision": "962baec5bcef8ab33b21",
+    "url": "/haydulski/Organizer-ToDo.git/static/css/main.c1588d2c.chunk.css"
+  },
+  {
+    "revision": "d26d2b5fde5881cc6a6b",
+    "url": "/haydulski/Organizer-ToDo.git/static/js/2.62deb98a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haydulski/Organizer-ToDo.git/static/js/2.62deb98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962baec5bcef8ab33b21",
+    "url": "/haydulski/Organizer-ToDo.git/static/js/main.388a34cd.chunk.js"
+  },
+  {
+    "revision": "818c9e33ff8c67151c6f",
+    "url": "/haydulski/Organizer-ToDo.git/static/js/runtime-main.6b35bfe7.js"
+  }
+]);
